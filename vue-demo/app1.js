c8e8ae47a46ed5b885c319d92f60e75549394ed4
flyingsnow0106 <7572320@qq.com>
2017-01-16 22:46:30 +0800

    learn at react-component 3

---
+++ vue-demo/app1.js
@@ -0,0 +1,9 @@
+/**
+ * Created by snow on 2017/1/16.
+ */
+var journal = new Vue({
+    el: '#journal',
+    data:{
+        message: 'your first entry'
+    }
+});
---
+++ vue-demo/app3.js
@@ -0,0 +1,15 @@
+/**
+ * Created by snow on 2017/1/16.
+ */
+new Vue({
+    el: '#app',
+    data: {
+        counter: 0
+    },
+    methods: {
+        increment(){
+            this.counter++;
+        }
+    }
+
+})
